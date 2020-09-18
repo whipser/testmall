82267d8e2de32ba07f9c083f6290bcd2f8fecfa4
unknown <kongpy@sitco.com>
2020-09-18 14:19:36 +0800

    tabbar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-bar-item" @click="itemclick">
+    <!-- <img src="../../assets/img/tabbar/icon01.png" alt=""> -->
+    <slot v-if="isActive" name="item-icon-active"></slot>
+    <slot v-else name="item-icon"></slot>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBarItem',
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: 'red'
+      }
+    },
+    data() {
+      return {
+        // isActive: true
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods:{
+      itemclick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+</style>
